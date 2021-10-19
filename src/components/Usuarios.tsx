00bdf219d9e4cea478ea605c4749fa4e24d2c779
Jairo Alzate <jairo.alzate@siesa.com>
2021-10-19 14:20:45 -0500

    add conection to api users

---
+++ src/components/Usuarios.tsx
@@ -0,0 +1,29 @@
+import { useEffect } from "react";
+import { reqResApi } from "../api/reqRes";
+import { ReqResListado } from "../interfaces/reqRes";
+export const Usuarios = () => {
+  useEffect(() => {
+    reqResApi
+      .get<ReqResListado>("/users")
+      .then((res) => {
+        console.log(res.data.data);
+      })
+      .catch((err) => console.log(err));
+  }, []);
+
+  return (
+    <>
+      <h3>Usuarios</h3>
+      <table className="table">
+        <thead>
+          <tr>
+            <th>Avatar</th>
+            <th>Nombre</th>
+            <th>Email</th>
+          </tr>
+        </thead>
+        <tbody></tbody>
+      </table>
+    </>
+  );
+};
