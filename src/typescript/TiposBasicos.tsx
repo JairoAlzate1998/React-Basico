e05f95d792bb12614e55cc85137931b050ee805f
Jairo Alzate <jairo.alzate@siesa.com>
2021-10-19 12:05:30 -0500

    react basic

---
+++ src/typescript/TiposBasicos.tsx
@@ -0,0 +1,16 @@
+export const TiposBasicos = () => {
+  const nombre: string  = "Fernando";
+  const edad: number = 35;
+  const estaActivo: boolean = false;
+
+  const poderes: string[] = ['velocidad', 'volar', 'respirar en el agua'];
+
+  return (
+    <>
+      <h3>Tipos Basicos</h3>
+      {nombre}, {edad}, {(estaActivo) ? 'activo': 'no activo'}
+      <br />
+      { poderes.join(', ') }
+    </>
+  );
+};
