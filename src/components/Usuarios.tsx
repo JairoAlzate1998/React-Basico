05c9611f2dc4dd13ed21819e47f3691666bd611d
Jairo Alzate <jairo.alzate@siesa.com>
2021-10-19 14:48:40 -0500

    add custom hook to usuarios

--- src/components/Usuarios.tsx
+++ src/components/Usuarios.tsx
@@ -1,28 +1,8 @@
-import { useEffect, useRef, useState } from "react";
-import { reqResApi } from "../api/reqRes";
-import { ReqResListado, Usuario } from "../interfaces/reqRes";
+import { useUsuarios } from '../Hooks/useUsuarios';
+import { Usuario } from '../interfaces/reqRes';
 export const Usuarios = () => {
-  const [usuarios, setUsuarios] = useState<Usuario[]>([]);
-
-  const paginaRef = useRef(1);
-
-  useEffect(() => {
-    cargarUsuarios();
-  }, []);
-
-  const cargarUsuarios = async () => {
-    const resp = await reqResApi.get<ReqResListado>("/users", {
-      params: {
-        page: paginaRef.current,
-      },
-    });
-    if (resp.data.data.length > 0) {
-      setUsuarios(resp.data.data);
-      paginaRef.current++;
-    } else {
-      alert("No hay mas registros");
-    }
-  };
+  
+  const {cargarUsuarios, usuarios} = useUsuarios();
 
   const renderItem = ({
     id,
